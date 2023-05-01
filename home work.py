a878dc70f44a189262b42380b823cb52a8de78f4
Majid Khamis <majid111111mm@gmail.com>
2023-05-01 23:51:36 +0400

    modifying 1

--- home work.py
+++ home work.py
@@ -1,8 +1,9 @@
+#------------------- السوال الاول  الجزئية رقم واحد -----------------------
 def aab(x,y):
-    odd_num=[]
-    even_num=[]
+    odd_num=[]      # list الفردي
+    even_num=[]     # list  الزوجي
     for num in range (x,y):
-        if num % 2 == 0:
+        if num % 2 == 0:        # هذا المعادلة تستخدم للحصول على رقم زوجي
             even_num.append(num)
         else:
             odd_num.append(num)
